31881a9a0426efa5d1385911c065d70f0003fd4d
Louis Stambolic <louis.stambolic@outlook.fr>
2023-11-26 19:03:42 +0100

    Ajustement des routes de l'API

---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const express = require('express');
+const router = express.Router();
+const booksCtrl = require('../controllers/books');
+
+
+
+
+
+router.get('', booksCtrl.getAllBooks);
+router.get('/bestrating', booksCtrl.getBestRatingBooks);
+router.post('', auth, multer, booksCtrl.createBook);
+router.get('/:id', booksCtrl.getOneBook);
+router.post('/:id/rating', auth, booksCtrl.addRatingToBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+
+
+
+module.exports = router;
