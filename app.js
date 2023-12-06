13da8be1467288272ee632cbe406962ca8f69810
Louis Stambolic <louis.stambolic@outlook.fr>
2023-12-06 20:17:17 +0100

    Ajout des variables d'environnement

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -6,25 +7,23 @@ const booksRoutes = require('./routes/books');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
-mongoose.connect('mongodb+srv://SLouka06:MandelieulaNapoule06@atlascluster.9xc5fx4.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
-
-
+const corsOrigin = process.env.CORS_ORIGIN || '*';
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', corsOrigin);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-  
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-  app.use('/api/books', booksRoutes);
-  app.use('/api/auth', userRoutes);
-  
+});
+
+app.use(process.env.IMAGES_ROUTE, express.static(path.join(__dirname, 'images')));
+app.use(process.env.BOOKS_ROUTE, booksRoutes);
+app.use(process.env.AUTH_ROUTE, userRoutes);
+
 
 module.exports = app;
