7640568e0fa4d74f21cde474ff39b4012f4ab5f7
SLouka06 <louis.stambolic@outlook.fr>
2023-12-09 02:55:59 +0100

    Ajout du dossier backend

---
+++ backend/routes/books.js
@@ -0,0 +1,20 @@
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const express = require('express');
+const router = express.Router();
+const booksCtrl = require('../controllers/books');
+
+
+
+
+
+router.get('', booksCtrl.getAllBooks);
+router.get('/bestrating', booksCtrl.getBestRatingBooks);
+router.post('', auth, multer, booksCtrl.createBook);
+router.get('/:id', booksCtrl.getOneBook);
+router.put('/:id', auth, multer, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+router.post('/:id/rating', auth, booksCtrl.addRatingToBook);
+
+
+module.exports = router;
